d6e316b9b5cc9fae1f6278b89270a78baa0037cd
Jeromey Klein <jiklein@ucsd.edu>
2018-10-13 17:55:48 -0700

    Updated sendEnvelope.js and HTML

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded( { extended:true}));
 
 app.use(express.static('public'));
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
 
 app.post('/submit_redirect', function (req, res) {
-  
-  var name = req.body.name;
-  var email = req.body.email;
-  sendEnvelope.SendEmail(name,email, app);
-  res.redirect('/');
-})
+
+    var name = req.body.name;
+    var email = req.body.email;
+    console.log("inside redirect: "+name);
+
+    sendEnvelope.SendEmail(name, email, app);
+    res.redirect('/');
+
+    })
 
 var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
 
-})
+    })
--- sendEnvelope.js
+++ sendEnvelope.js
@@ -1,136 +1,99 @@
-const express = require('express');
 const docusign = require('docusign-esign');
 const path = require('path');
 const apiClient = new docusign.ApiClient();
-//const app = express();
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
 const fs = require('fs');
-
-
-//On execution an envelope is sent to the provided email address, one signHere
-//tab is added, the document supplied in workingdirectory\fileName is used.
-//Open a new browser pointed at http://localhost:3000 to execute. 
-//-------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------
-
-//Fill in Variables Here
-
-//Obtain an OAuth token from https://developers.docusign.com/oauth-token-generator
-//Obtain your accountId from account-d.docusign.com > Go To Admin > API and Keys
 
 const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAAnjG3VDHWSAgAAN5UxZcx1kgCACuQm0Cp8u9KlKQZXmM4xX4VAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACAjSCTUTHWSA.eEbgcppFieE-KyWadK4mNOXNetNc0n3eqW1BI3Vn529tRoK3S98C2e9TH9S2WUplJ_gtcgdhafQQNvpPdDz4XNFFswF-5PwQtHixNngI56jwU9NOHOMgXm62XNENAq28VC9EwgNd6T78sBcC8phWid9bZh4P6oeBp_NRdLmGwDrNPGpJDpqueK1NNb3INhqxJvxwobeGDIIqQt6Q6IfCfBo8yTrDXxaint1cpem7C5QamnvuMKPXvEXmMqFemPV-bpvcBxNWZ3SAQcK8zcUllH86pcp51kqthi1a0rh-7iyhDiSKasVjVgf27bZw-WeDphkmtbxdUXxtybZBVypk_w';
 const accountId = '6814377';
 
-
-//Recipient Information goes here
-const recipientName = 'Kevin';
-const recipientEmail = 'Kevin.Medzor@gmail.com';
-
-//Point this to the document you wish to send's location on the local machine. Default location is __workingDir\fileName
 const fileName = 'docs/House.pdf'; //IE: test.pdf
-//-------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------
-
-
-/*app.listen(port, host, function (err) {
-    if (err) {
-        return res.send('Error while starting the server:' + err);
-    }
-
-    console.log('Your server is running on http://' + host + ':' + port + '.');
-});*/
 
 function SendEmail(name, email, app){
-    app.get('/', function (req, res) {
-    apiClient.setBasePath('https://demo.docusign.net/restapi');
-    apiClient.addDefaultHeader('Authorization', 'Bearer ' + OAuthToken);
 
-    // *** Begin envelope creation ***
-	console.log(name + "  " + email);
+  apiClient.setBasePath('https://demo.docusign.net/restapi');
+  apiClient.addDefaultHeader('Authorization', 'Bearer ' + OAuthToken);
 
-    //Read the file you wish to send from the local machine.
-    fileStream = process.argv[2];
-    pdfBytes = fs.readFileSync(path.resolve(__dirname, fileName));
-    pdfBase64 = pdfBytes.toString('base64');
+  // *** Begin envelope creation ***
 
-    docusign.Configuration.default.setDefaultApiClient(apiClient);
+  //Read the file you wish to send from the local machine.
+  fileStream = process.argv[2];
+  pdfBytes = fs.readFileSync(path.resolve(__dirname, fileName));
+  pdfBase64 = pdfBytes.toString('base64');
 
-    var envDef = new docusign.EnvelopeDefinition();
+  docusign.Configuration.default.setDefaultApiClient(apiClient);
 
-    //Set the Email Subject line and email message
-    envDef.emailSubject = 'Please sign this document sent from Node SDK';
-    envDef.emailBlurb = 'Please sign this document sent from the DocuSign Node.JS SDK.'
+  var envDef = new docusign.EnvelopeDefinition();
 
-        //Read the file from the document and convert it to a Base64String
-        var doc = new docusign.Document();
-    doc.documentBase64 = pdfBase64;
-    doc.fileExtension = 'pdf';
-    doc.name = 'Node Doc Send Sample';
-    doc.documentId = '1';
+  //Set the Email Subject line and email message
+  envDef.emailSubject = 'Sign this agreement from eAgree.';
+  envDef.emailBlurb = 'Please sign this agreement.'
 
-    //Push the doc to the documents array.
-    var docs = [];
-    docs.push(doc);
-    envDef.documents = docs;
+  //Read the file from the document and convert it to a Base64String
+  var doc = new docusign.Document();
+  doc.documentBase64 = pdfBase64;
+  doc.fileExtension = 'pdf';
+  doc.name = 'Node Doc Send Sample';
+  doc.documentId = '1';
 
-    //Create the signer with the previously provided name / email address
-    var signer = new docusign.Signer();
-    signer.name = name;
-    signer.email = email;
-    signer.routingOrder = '1';
-    signer.recipientId = '1';
+  //Push the doc to the documents array.
+  var docs = [];
+  docs.push(doc);
+  envDef.documents = docs;
 
-    //Create a tabs object and a signHere tab to be placed on the envelope
-    var tabs = new docusign.Tabs();
+  //Create the signer with the previously provided name / email address
+  var signer = new docusign.Signer();
+  signer.name = name;
+  signer.email = email;
+  signer.routingOrder = '1';
+  signer.recipientId = '1';
 
-    var signHere = new docusign.SignHere();
-    signHere.documentId = '1';
-    signHere.pageNumber = '1';
-    signHere.recipientId = '1';
-    signHere.tabLabel = 'SignHereTab';
-    signHere.xPosition = '50';
-    signHere.yPosition = '50';
+  //Create a tabs object and a signHere tab to be placed on the envelope
+  var tabs = new docusign.Tabs();
 
-    //Create the array for SignHere tabs, then add it to the general tab array
-    signHereTabArray = [];
-    signHereTabArray.push(signHere);
+  var signHere = new docusign.SignHere();
+  signHere.documentId = '1';
+  signHere.pageNumber = '1';
+  signHere.recipientId = '1';
+  signHere.tabLabel = 'SignHereTab';
+  signHere.xPosition = '50';
+  signHere.yPosition = '50';
 
-    tabs.signHereTabs = signHereTabArray;
+  //Create the array for SignHere tabs, then add it to the general tab array
+  signHereTabArray = [];
+  signHereTabArray.push(signHere);
 
-    //Then set the recipient, named signer, tabs to the previously created tab array
-    signer.tabs = tabs;
+  tabs.signHereTabs = signHereTabArray;
 
-    //Add the signer to the signers array
-    var signers = [];
-    signers.push(signer);
+  //Then set the recipient, named signer, tabs to the previously created tab array
+  signer.tabs = tabs;
 
-    //Envelope status for drafts is created, set to sent if wanting to send the envelope right away
-    envDef.status = 'sent';
+  //Add the signer to the signers array
+  var signers = [];
+  signers.push(signer);
 
-    //Create the general recipients object, then set the signers to the signer array just created
-    var recipients = new docusign.Recipients();
-    recipients.signers = signers;
+  //Envelope status for drafts is created, set to sent if wanting to send the envelope right away
+  envDef.status = 'sent';
 
-    //Then add the recipients object to the enevelope definitions
-    envDef.recipients = recipients;
+  //Create the general recipients object, then set the signers to the signer array just created
+  var recipients = new docusign.Recipients();
+  recipients.signers = signers;
 
-    // *** End envelope creation ***
+  //Then add the recipients object to the enevelope definitions
+  envDef.recipients = recipients;
 
+  // *** End envelope creation ***
 
-    //Send the envelope
-    var envelopesApi = new docusign.EnvelopesApi();
-    envelopesApi.createEnvelope(accountId, { 'envelopeDefinition': envDef }, function (err, envelopeSummary, response) {
+  //Send the envelope
+  var envelopesApi = new docusign.EnvelopesApi();
+  envelopesApi.createEnvelope(accountId, { 'envelopeDefinition': envDef }, function (err, envelopeSummary, response) {
 
-        if (err) {
- 		console.log(response);
-            return res.send('Error while sending a DocuSign envelope:' + err);
-        }
+      if (err) {
+      console.log(response);
+      return res.send('Error while sending a DocuSign envelope:' + err);
+      }
 
-        res.send(envelopeSummary);
-
-    });
-}); 
+      });
 
 }
+
 module.exports = { SendEmail } ;
