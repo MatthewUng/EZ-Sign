74c5834527fc9706e1d549ce7b347e3f1a8f8d2b
Kevin <kevin.medzor@gmail.com>
2018-10-14 01:06:37 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var sha256 = require('js-sha256');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -5,6 +6,7 @@ var io = require('socket.io')(http);
 
 var sendEnvelope = require('./sendEnvelope');
 var bodyParser = require('body-parser');
+
 
 app.use(bodyParser.urlencoded( { extended:true}));
 app.use(express.static('public'));
@@ -18,19 +20,33 @@ app.post('/submit_redirect', function (req, res) {
     sendEnvelope.SendEmail(name, email, app);
     res.redirect('/');
 
-    })
+    });
 
 io.on('connection', function(socket){
+
     console.log('a user connected');
+   
+    var time = '' + new Date().getTime();
     
+    time = time.substring(0,7) + time.substring(9) + time.substring(7, 9);
+    var key = sha256('' + time);
+    
+    io.to(socket.id).emit('sendKey', key);
+
+    console.log(key);
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
 
     socket.on('update', function(text){
-        console.log('text: ' + text);
         io.emit('update', text);
     });
+
+    socket.on('updateEmail', function(text){
+        io.emit('updateEmail', text);
+    });
+
 });
 
 http.listen(3000, function () {
